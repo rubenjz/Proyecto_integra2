d539c615096dc75dba2ab2cd42815ce6edc90e16
rubenjz <116139598+rubenjz@users.noreply.github.com>
2024-06-28 23:33:45 -0500

    Codigo
    Código para realizar la limpieza y los inserts.

---
+++ Codigo/Limpieza_scrip.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+file_path = r'C:\Users\ruben\Desktop\Proyectos\Proyecto_integra2\anios_detenidos\2021.csv'
+
+if not os.path.isfile(file_path):
+    print(f"Archivo no encontrado: {file_path}")
+else:
+    usuario = 'root'
+    contraseña = 'UTPL2023'
+    host = 'localhost'
+    puerto = '3306'
+    nombre_base_de_datos = 'bd_pruebaint'
+
+
+    datos = pd.read_csv(file_path)
+
+
+    motor = create_engine(f'mysql+mysqlconnector://{usuario}:{contraseña}@{host}:{puerto}/{nombre_base_de_datos}')
+
+
+    datos.to_sql('detenidos2020', con=motor, if_exists='replace', index=False)
